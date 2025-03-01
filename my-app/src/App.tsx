27b9d91321601a681135ced133878fb126812abf
Ember Kerstetter <emberk@udel.edu>
2025-03-01 17:38:06 -0500

    watched https://www.youtube.com/watch?v=uDCBSnWkuH0 and created a skeleton form with 3 pages, back and next button, fields for user input, and a message that confirms the form submission. We can continue to edit and expand from this basic template and find a way to connect it to our home page.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,70 +1,97 @@
-//import logo from './logo.svg';
-import React from 'react';
-import './App.css';
-import { useMultistepForm } from './useMultistepForm';
-import { AddressForm, DoctorPersonalInfo, UserName } from './Questions';
+import React, { FormEvent, useState } from "react";
+import "./App.css";
+import { useMultistepForm } from "./Components/useMultistepForm";
+import {
+  AddressForm,
+  DoctorPersonalInfo,
+  UserName,
+} from "./Components/Questions";
 
-/*
-//import logo from './logo.svg';
-import './App.css';
-import { useMultistepForm } from './useMultistepForm';
-import React from 'react';
+type FormData = {
+  firstName: string;
+  lastName: string;
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+  age: string;
+  gender: string;
+  language: string;
+};
+
+const INITIAL_DATA: FormData = {
+  firstName: "",
+  lastName: "",
+  street: "",
+  city: "",
+  state: "",
+  zip: "",
+  age: "",
+  gender: "",
+  language: "",
+};
 
 function App() {
+  //create the state
+  const [data, setData] = useState(INITIAL_DATA); //stores the data in state, so it is saved between button clicks
+  //allow fields like name and age to be updated
+  //TypeScript allows any fields of a type to be passed in optionally when using Partial<>
+  function updateFields(fields: Partial<FormData>) {
+    setData((prev) => {
+      return { ...prev, ...fields };
+    });
+  }
+  //implement the hook
+  const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } =
+    useMultistepForm([
+      <UserName {...data} updateFields={updateFields} />,
+      <AddressForm {...data} updateFields={updateFields} />,
+      <DoctorPersonalInfo {...data} updateFields={updateFields} />,
+    ]); //each form takes in info
+
+  //handle button submission between pages
+  function onSubmit(e: FormEvent) {
+    e.preventDefault(); //prevents page from doing an automatic refresh/total form submit option
+    if (!isLastStep) return next(); // only go to next page if we aren't on last page
+    alert("Successful Submission of Form"); //what to do if the finish button is clicked
+  }
+  //other stuff
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        position: "relative",
+        background: "white",
+        border: "1px solid black",
+        padding: "2rem",
+        margin: "1rem",
+        borderRadius: ".5rem",
+        fontFamily: "Arial",
+        maxWidth: "max-content",
+      }}
+    >
+      <form onSubmit={onSubmit}>
+        <div style={{ position: "absolute", top: ".5rem", right: ".5rem" }}>
+          {currentStepIndex + 1} / {steps.length}
+        </div>
+        {step}
+        <div
+          style={{
+            marginTop: "1rem",
+            display: "flex",
+            gap: ".5rem",
+            justifyContent: "flex-end",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {!isFirstStep && (
+            <button type="button" onClick={back}>
+              Back
+            </button>
+          )}
+          <button type="submit">{isLastStep ? "Finish" : "Next"}</button>
+        </div>
+      </form>
     </div>
   );
 }
-  */
-
-function App(){
-  //implement the hook
-  const {
-    steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } = useMultistepForm([<UserName/>, <AddressForm/>, <DoctorPersonalInfo/>])
-  //other stuff
-  return <div style={{
-    position: "relative",
-    background: "white",
-    border: "1px solid black",
-    padding: "2rem",
-    margin: "1rem",
-    borderRadius: ".5rem",
-    fontFamily: "Arial"
-  }}
-  >
-    <form>
-      <div style={{ position: "absolute", top: ".5rem", right: ".5rem"}} >
-        {currentStepIndex + 1} / {steps.length} 
-      </div>
-      {step}
-      <div 
-        style={{marginTop: "1rem", display: "flex", gap: ".5rem", justifyContent: "flex-end"
-
-        }}
-      >
-        {!isFirstStep && <button type="button" onClick={back}>Back</button>}
-        <button type="button" onClick={next}>
-          {isLastStep ? "Finish" : "Next"}
-        </button>
-      </div>
-    </form>
-  </div>
-}
-
 
 export default App;
--- my-app/src/Components/UserForm.tsx
+++ my-app/src/Components/UserForm.tsx
@@ -1,14 +1,14 @@
 import { FormWrapper } from "./FormWrapper";
 
-export function UserForm(){
-  return(
+export function UserForm() {
+  return (
     <FormWrapper title="Home Page">
       <label>First Name</label>
-      <input autoFocus required type="text"/>
+      <input autoFocus required type="text" />
       <label>Last Name</label>
-      <input required type="text"/>
+      <input required type="text" />
       <label>Age</label>
-      <input required min={1} type="number"/>
+      <input required min={1} type="number" />
     </FormWrapper>
-  )
+  );
 }
